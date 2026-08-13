cd2070fc71dda0ecc1aa65f0d0c7599e76cbc766
Sidd2004Shukla <siddharth.shuklapti@gmail.com>
2026-08-13 18:11:04 +0530

    fixing the ruff

--- alembic/versions/02a862f9ae85_initial_migration.py
+++ alembic/versions/02a862f9ae85_initial_migration.py
@@ -6,17 +6,18 @@ Create Date: 2026-08-13 14:15:39.920958
 
 """
 
-from collections.abc import Sequence
 from typing import Union
+from collections.abc import Sequence
 
 import sqlalchemy as sa
+
 from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "02a862f9ae85"
-down_revision: Union[str, Sequence[str], None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = None
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
